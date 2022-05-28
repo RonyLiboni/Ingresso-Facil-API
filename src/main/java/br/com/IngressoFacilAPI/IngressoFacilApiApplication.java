7b87e05afe86b90ad84a330ebafbe3707025967d
RonyLiboni <ronald.liboni@acad.pucrs.br>
2022-05-28 09:30:39 -0300

    primeiro commit

---
+++ src/main/java/br/com/IngressoFacilAPI/IngressoFacilApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.IngressoFacilAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngressoFacilApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngressoFacilApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/IngressoFacilAPI/IngressoFacilApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.IngressoFacilAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngressoFacilApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
